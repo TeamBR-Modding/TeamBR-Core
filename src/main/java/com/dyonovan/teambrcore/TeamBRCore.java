ced556429cb7e006ab474e959271eb44bdc87bd3
pauljoda <pauldavis101@gmail.com>
2015-03-06 17:26:15 -0600

    Notifications
    [ci JBUILD]

--- src/main/java/com/dyonovan/teambrcore/TeamBRCore.java
+++ src/main/java/com/dyonovan/teambrcore/TeamBRCore.java
@@ -1,12 +1,23 @@
 package com.dyonovan.teambrcore;
 
+import com.dyonovan.teambrcore.helpers.KeyInputHelper;
 import com.dyonovan.teambrcore.lib.Constants;
+import com.dyonovan.teambrcore.managers.GuiManager;
+import com.dyonovan.teambrcore.notification.NotificationHelper;
+import com.dyonovan.teambrcore.notification.NotificationKeyBinding;
+import com.dyonovan.teambrcore.notification.NotificationTickHandler;
+import cpw.mods.fml.common.FMLCommonHandler;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.Instance;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.network.NetworkRegistry;
+import cpw.mods.fml.relauncher.Side;
+import net.minecraftforge.common.config.Configuration;
+
+import java.io.File;
 
 @Mod(name = Constants.MODNAME, modid = Constants.MODID, version = Constants.VERSION)
 
@@ -18,12 +29,23 @@ public class TeamBRCore {
 
     @EventHandler
     public void preInit(FMLPreInitializationEvent event){
+        if(event.getSide() == Side.CLIENT) {
+            FMLCommonHandler.instance().bus().register(new NotificationTickHandler());
 
+            NotificationHelper.notificationConfig = new Configuration(new File(event.getModConfigurationDirectory().getAbsolutePath() + File.separator  + "TeamBR" + File.separator + "NotificationsSettings.cfg"));
+            NotificationHelper.notificationXPos = NotificationHelper.notificationConfig.getInt("notification xpos", "notifications", 1, 0, 2, "0: Left\n1: Center\n2: Right");
+            NotificationHelper.notificationConfig.save();
+        }
     }
 
     @EventHandler
     public void init(FMLInitializationEvent event) {
-
+        if(event.getSide() == Side.CLIENT)
+        {
+            NotificationKeyBinding.init();
+        }
+        NetworkRegistry.INSTANCE.registerGuiHandler(instance, new GuiManager());
+        FMLCommonHandler.instance().bus().register(new KeyInputHelper());
     }
 
     @EventHandler
