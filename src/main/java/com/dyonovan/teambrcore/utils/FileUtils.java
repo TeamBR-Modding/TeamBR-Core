16515012e1bc2eea354a4ae6af640ecfae7a3b3e
Dyonovan <mcbetacraft@gmail.com>
2015-03-11 00:56:38 -0400

    Small tweak

--- src/main/java/com/dyonovan/teambrcore/utils/FileUtils.java
+++ src/main/java/com/dyonovan/teambrcore/utils/FileUtils.java
@@ -11,7 +11,6 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Enumeration;
-import java.util.Iterator;
 import java.util.jar.JarEntry;
 
 @SuppressWarnings("unused")
